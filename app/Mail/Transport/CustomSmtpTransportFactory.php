9f3dd58873b8cae54107c047eb8d4f8c13d26aed
AlbertWebs <albertmuhatia@gmail.com>
2026-02-05 13:35:40 +0300

    up

--- app/Mail/Transport/CustomSmtpTransportFactory.php
+++ app/Mail/Transport/CustomSmtpTransportFactory.php
@@ -7,6 +7,7 @@ use Symfony\Component\Mailer\Transport\Dsn;
 use Symfony\Component\Mailer\Transport\TransportInterface;
 use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
 use Symfony\Component\Mailer\Transport\Smtp\Stream\SocketStream;
+use App\Mail\Stream\CustomSocketStream;
 
 class CustomSmtpTransportFactory implements Transport\TransportFactoryInterface
 {
@@ -30,52 +31,36 @@ class CustomSmtpTransportFactory implements Transport\TransportFactoryInterface
             $sslOptions = config('mail.mailers.smtp.stream.ssl', []);
         }
         
-        $transport = new EsmtpTransport($host, $port, false);
+        // Create custom stream with SSL options if needed
+        $stream = null;
+        if (!empty($sslOptions)) {
+            $customStream = new CustomSocketStream();
+            $customStream->setHost($host);
+            $customStream->setPort($port);
+            $customStream->setStreamOptions(['ssl' => $sslOptions]);
+            $stream = $customStream;
+            
+            \Log::info('Using custom socket stream with SSL options', [
+                'mailer' => $mailer,
+                'host' => $host,
+                'ssl_options' => $sslOptions
+            ]);
+        }
+        
+        // Create transport with custom stream (or default if no SSL options)
+        $transport = new EsmtpTransport($host, $port, false, null, null, $stream);
         
         if ($username && $password) {
             $transport->setUsername($username);
             $transport->setPassword($password);
         }
         
-        // Set SSL options on the stream BEFORE it's initialized
-        if (!empty($sslOptions)) {
-            try {
-                // Ensure global stream context is also set as fallback
-                $currentContext = stream_context_get_default();
-                $currentOptions = $currentContext ? stream_context_get_options($currentContext) : [];
-                $mergedOptions = array_merge_recursive($currentOptions, ['ssl' => $sslOptions]);
-                stream_context_set_default($mergedOptions);
-                
-                // Get the stream and set options before any initialization
-                $stream = $transport->getStream();
-                if ($stream instanceof SocketStream) {
-                    // Set stream options - these will be used when initialize() is called
-                    // The options should be at the root level with 'ssl' key
-                    $stream->setStreamOptions(['ssl' => $sslOptions]);
-                    
-                    // Verify the options were set
-                    $setOptions = $stream->getStreamOptions();
-                    \Log::info('SSL options set on SMTP stream', [
-                        'mailer' => $mailer,
-                        'host' => $host,
-                        'ssl_options_set' => $sslOptions,
-                        'stream_options_retrieved' => $setOptions
-                    ]);
-                }
-            } catch (\Exception $e) {
-                \Log::error('Failed to set SSL options on SMTP stream', [
-                    'error' => $e->getMessage(),
-                    'mailer' => $mailer,
-                    'host' => $host,
-                    'ssl_options' => $sslOptions,
-                    'trace' => $e->getTraceAsString()
-                ]);
+        // Fallback: if we didn't use custom stream, set options on default stream
+        if (empty($stream) && !empty($sslOptions)) {
+            $defaultStream = $transport->getStream();
+            if ($defaultStream instanceof SocketStream) {
+                $defaultStream->setStreamOptions(['ssl' => $sslOptions]);
             }
-        } else {
-            \Log::warning('No SSL options found for mailer', [
-                'mailer' => $mailer,
-                'mailer_config' => $mailerConfig ?? 'not found'
-            ]);
         }
         
         return $transport;
