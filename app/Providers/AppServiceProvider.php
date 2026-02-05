322bf4eb1329ff6b2190c51f8b7ffe02c583ecad
AlbertWebs <albertmuhatia@gmail.com>
2026-02-05 11:53:56 +0300

    upate maisls

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Mail\MailManager;
+use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
+use Symfony\Component\Mailer\Transport\Smtp\Stream\SocketStream;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -16,22 +19,18 @@ class AppServiceProvider extends ServiceProvider
         $verifyPeerName = env('MAIL_VERIFY_PEER_NAME');
         if ($verifyPeerName === false || $verifyPeerName === 'false' || $verifyPeerName === '0') {
             $sslOptions = [
+                'verify_peer' => false,
                 'verify_peer_name' => false,
-                'verify_peer' => (env('MAIL_VERIFY_PEER') === false || env('MAIL_VERIFY_PEER') === 'false' || env('MAIL_VERIFY_PEER') === '0') ? false : true,
-                'allow_self_signed' => true, // Allow self-signed certificates from proxy
+                'allow_self_signed' => true,
             ];
             
             $peerName = env('MAIL_PEER_NAME');
             if ($peerName) {
-                // When verify_peer_name is false, peer_name should still be set to match proxy's CN
                 $sslOptions['peer_name'] = $peerName;
             }
             
-            // Set default stream context with SSL options - use array_replace_recursive to override
-            $currentContext = stream_context_get_default();
-            $currentOptions = $currentContext ? stream_context_get_options($currentContext) : [];
-            $newOptions = array_replace_recursive($currentOptions, ['ssl' => $sslOptions]);
-            stream_context_set_default($newOptions);
+            // Set default stream context with SSL options
+            stream_context_set_default(['ssl' => $sslOptions]);
         }
     }
 
@@ -40,6 +39,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // The SSL options are already set in bootstrap/app.php and register()
+        // The config file also has the SSL options which Symfony Mailer should read
+        // If it still doesn't work, the global stream context set in register() should handle it
     }
 }
