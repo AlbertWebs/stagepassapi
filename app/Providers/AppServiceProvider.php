b5a8fadcc769ed2e620d765924ab4e152a683bbf
AlbertWebs <albertmuhatia@gmail.com>
2026-02-05 11:43:26 +0300

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,7 +11,26 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Set SSL stream context options EARLY for SMTP connections behind proxy
+        // This ensures SSL options are applied before any mail connection is attempted
+        $verifyPeerName = env('MAIL_VERIFY_PEER_NAME');
+        if ($verifyPeerName === false || $verifyPeerName === 'false' || $verifyPeerName === '0') {
+            $sslOptions = [
+                'verify_peer_name' => false,
+                'verify_peer' => (env('MAIL_VERIFY_PEER') === false || env('MAIL_VERIFY_PEER') === 'false' || env('MAIL_VERIFY_PEER') === '0') ? false : true,
+            ];
+            
+            $peerName = env('MAIL_PEER_NAME');
+            if ($peerName) {
+                $sslOptions['peer_name'] = $peerName;
+            }
+            
+            // Set default stream context with SSL options
+            $currentContext = stream_context_get_default();
+            $currentOptions = $currentContext ? stream_context_get_options($currentContext) : [];
+            $newOptions = array_merge_recursive($currentOptions, ['ssl' => $sslOptions]);
+            stream_context_set_default($newOptions);
+        }
     }
 
     /**
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,34 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
+// Set SSL stream context options VERY EARLY for SMTP connections behind proxy
+// This is needed for cPanel servers where proxy intercepts SMTP connections
+// Set it at the PHP level before Laravel even starts
+$verifyPeerName = getenv('MAIL_VERIFY_PEER_NAME');
+if ($verifyPeerName === false || $verifyPeerName === 'false' || $verifyPeerName === '0') {
+    $sslOptions = [
+        'verify_peer_name' => false,
+        'verify_peer' => (getenv('MAIL_VERIFY_PEER') === false || getenv('MAIL_VERIFY_PEER') === 'false' || getenv('MAIL_VERIFY_PEER') === '0') ? false : true,
+    ];
+    
+    $peerName = getenv('MAIL_PEER_NAME');
+    if ($peerName) {
+        $sslOptions['peer_name'] = $peerName;
+    }
+    
+    if (!empty($sslOptions)) {
+        // Get current default context options
+        $currentContext = stream_context_get_default();
+        $currentOptions = $currentContext ? stream_context_get_options($currentContext) : [];
+        
+        // Merge SSL options
+        $newOptions = array_merge_recursive($currentOptions, ['ssl' => $sslOptions]);
+        
+        // Set default context with merged options
+        stream_context_set_default($newOptions);
+    }
+}
+
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
