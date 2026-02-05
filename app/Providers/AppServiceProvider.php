727e6e2d92e2b230c7ff33421f510b1dd99c02e0
AlbertWebs <albertmuhatia@gmail.com>
2026-02-05 11:29:16 +0300

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Mail\MailManager;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,23 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Ensure SSL options from config are applied to SMTP transport
+        if (config('mail.default') === 'smtp' && !empty(config('mail.mailers.smtp.stream.ssl'))) {
+            $this->app->afterResolving(MailManager::class, function (MailManager $mailManager) {
+                $mailManager->extend('smtp', function (array $config) use ($mailManager) {
+                    // Ensure SSL options from config are preserved
+                    if (isset($config['stream']['ssl'])) {
+                        $config['stream']['ssl'] = array_merge(
+                            config('mail.mailers.smtp.stream.ssl', []),
+                            $config['stream']['ssl']
+                        );
+                    } else {
+                        $config['stream']['ssl'] = config('mail.mailers.smtp.stream.ssl', []);
+                    }
+                    
+                    return $mailManager->createTransport($config);
+                });
+            });
+        }
     }
 }
